81f30bfa8ba6ccc1cdf5e21de9e3eaec6b86a679
Maksim Shishkov <mak51m.sh.personal@gmail.com>
2025-07-28 15:42:24 +0300

    fixed scaling blur

--- include/texture_manager.h
+++ include/texture_manager.h
@@ -3,17 +3,24 @@
 
 namespace TextureManager {
 
-[[nodiscard]] inline SDL_Texture* LoadTexture(SDL_Renderer* renderer, char* texture)
+[[nodiscard]] inline SDL_Texture* LoadTexture(SDL_Renderer* renderer, const char* texture)
 {
     SDL_Surface* tmpSurface = IMG_Load(texture);
+    if (!tmpSurface) {
+        spdlog::critical("Texture {} is not created", texture);
+        Log::CriticalSDLError("Surface is not loaded. {}\n");
+        return nullptr;
+    }
+
     SDL_Texture* textureFromSurface = SDL_CreateTextureFromSurface(renderer, tmpSurface);
     if (!textureFromSurface) {
-        spdlog::critical("Texture {} is not created");
+        spdlog::critical("Texture {} is not created", texture);
         Log::CriticalSDLError("{}\n");
     }
 
     SDL_DestroySurface(tmpSurface);
 
+    SDL_SetTextureScaleMode(textureFromSurface, SDL_SCALEMODE_NEAREST);
     return textureFromSurface;
 }
 
